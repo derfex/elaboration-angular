207678cf22dd1022c5ff380e32eef8c7e6fbdb52
derfex <dm1tr0@mail.ru>
2019-12-13 19:30:07 +0300

    feature. Products. Create component

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.sass'],
+})
+export class ProductsComponent implements OnInit {
+  products = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  getProducts() {
+    return this.products;
+  }
+}
