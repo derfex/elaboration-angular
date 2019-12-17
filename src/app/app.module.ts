58c82c53ad48941a92e485892e5ebed530f0dfb5
derfex <derfex@yandex.ru>
2019-12-17 20:57:05 +0300

    feature. Products. Use `HttpClientModule`. Use `products.json` instead `products.data.ts`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
