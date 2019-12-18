c8dc8fb74535d68f2d3917c40a8f818009d75980
derfex <derfex@yandex.ru>
2019-12-18 15:23:58 +0300

    DEV. Products. Use `app-products` in `app.component.html`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
