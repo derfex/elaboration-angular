4c03c7172f133125bcc0cbe4e99d8ab92dd6c6ea
derfex <derfex@yandex.ru>
2019-12-18 17:23:54 +0300

    DEV. feature. Products. Apply Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {
+  MatCheckboxModule,
+  MatTableModule,
+} from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
@@ -13,8 +18,11 @@ import { ProductsComponent } from './products/products.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    MatCheckboxModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
