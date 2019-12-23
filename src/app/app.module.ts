2ad8cf442e2df80cbc463c1d2e364ffa631d3e32
derfex <dm1tr0@mail.ru>
2019-12-24 02:01:51 +0300

    feature: Shop. Use component in `AppComponent`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductsComponent,
+    ShopComponent,
   ],
   imports: [
     BrowserModule,
