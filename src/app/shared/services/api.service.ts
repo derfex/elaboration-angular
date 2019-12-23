4cfb9f9151283eb5c905b8481fc03c146f778943
derfex <derfex@yandex.ru>
2019-12-23 20:14:21 +0300

    DEV. 1

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -9,6 +9,19 @@ export class ApiService {
   constructor(private httpClient: HttpClient) {
   }
 
+  /*
+  public post({url, data, resolve, reject}) {
+    this.httpClient.post(url, data)
+      .toPromise()
+      .then((response: any) => {
+        resolve(response);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  }
+  /**/
+
   public get<T>(url): Observable<T> {
     return this.httpClient.get<T>(url);
   }
