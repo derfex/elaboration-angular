b4578c646f87b91dc22ca92866aafb3e203f876d
derfex <dm1tr0@mail.ru>
2019-12-24 10:15:48 +0300

    feature: Products. Apply Material. Delete unused `ProductModel`

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,8 +1,10 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { environment } from 'src/environments/environment';
 
-interface IProductGridViewModel {
+interface IProductTableViewModel {
   id: number;
   name: string;
   parent: {
@@ -10,7 +12,6 @@ interface IProductGridViewModel {
     name: string;
   };
   price: number;
-  selected: boolean;
 }
 
 @Component({
@@ -19,7 +20,9 @@ interface IProductGridViewModel {
   styleUrls: ['./products.component.sass'],
 })
 export class ProductsComponent implements OnInit {
-  products: IProductGridViewModel[] = [];
+  dataSource: MatTableDataSource<IProductTableViewModel> = new MatTableDataSource<IProductTableViewModel>([]);
+  displayedColumns: string[] = ['select', 'number', 'name', 'parent', 'price'];
+  selection = new SelectionModel<IProductTableViewModel>(true, []);
 
   constructor(private http: HttpClient) {
   }
@@ -27,12 +30,16 @@ export class ProductsComponent implements OnInit {
   ngOnInit() {
     this.http.get(environment.API.products.getAll)
       .subscribe(
-        (data: any[]) => {
-          this.products = data
-            .map(item => ({
-              ...item,
-              selected: false,
-            }));
+        (data: IProductTableViewModel[]) => {
+          data.forEach(item => {
+            if (!item.parent) {
+              item.parent = {
+                id: null,
+                name: '—',
+              };
+            }
+          });
+          this.dataSource = new MatTableDataSource<IProductTableViewModel>(data);
         },
         error => {
           throw error;
@@ -40,7 +47,27 @@ export class ProductsComponent implements OnInit {
       );
   }
 
-  getProducts() {
-    return this.products;
+  // Whether the number of selected elements matches the total number of rows.
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  // Selects all rows if they are not all selected; otherwise clear selection.
+  masterToggle() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+    } else {
+      this.dataSource.data.forEach(row => this.selection.select(row));
+    }
+  }
+
+  // The label for the checkbox on the passed row.
+  checkboxLabel(row?: IProductTableViewModel): string {
+    if (!row) {
+      return `${ this.isAllSelected() ? 'select' : 'deselect' } all`;
+    }
+    return `${ this.selection.isSelected(row) ? 'deselect' : 'select' } row ${ row.id + 1 }`;
   }
 }
