4f90e42529dd0802c1eacb50ea45843fffc39389
derfex <derfex@yandex.ru>
2019-12-25 12:10:46 +0300

    feature: Products. Transform products

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 import { APIService } from 'src/app/shared/services/api.service';
+
+function transformProduct(product) {
+  if (!product.parent) {
+    product.parent = {
+      id: null,
+      name: '—',
+    };
+  }
+  return product;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +21,8 @@ export class ProductsService {
   }
 
   getAll() {
-    return this.apiService.get('assets/products.json');
+    return this.apiService
+      .get('assets/products.json')
+      .pipe(map((products: []) => products.map(transformProduct)));
   }
 }
