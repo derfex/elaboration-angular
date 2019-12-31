52244c96aed82a39a699a6e1b3338112f482fe2b
derfex <derfex@yandex.ru>
2019-12-31 13:57:19 +0300

    refactor: ShopModule. Create module, move dependencies from `AppModule` to `ShopModule`

---
+++ src/app/shop/shop.module.ts
@@ -0,0 +1,53 @@
+// External modules.
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatSelectModule,
+  MatSortModule,
+  MatTableModule,
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Main component.
+import { ShopComponent } from './shop.component';
+
+// Components.
+import { CartComponent } from './cart/cart.component';
+import { CategoriesSelectComponent } from './shared/components/categories-select/categories-select.component';
+import { ProductsComponent } from './products/products.component';
+
+
+@NgModule({
+  declarations: [
+    ShopComponent,
+
+    CartComponent,
+    CategoriesSelectComponent,
+    ProductsComponent,
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
+    MatSortModule,
+    MatTableModule,
+  ],
+  exports: [
+    ShopComponent,
+  ],
+  providers: [],
+})
+export class ShopModule {
+}
